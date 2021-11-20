1faa2c51f8397129874d9eaed3577f54b8842d15
Lina Dva <Anhelina.Pashkowets@gmail.com>
2021-11-20 12:50:36 +0300

    direct reading from xml is complete

--- src/com/company/TextArea.java
+++ src/com/company/TextArea.java
@@ -9,10 +9,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.lang.String;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TextArea extends JTextPane{
     private List<String> txt;
     private String path = "";
+    private final DefaultStyledDocument document;
+
     private void save() throws BadLocationException {
         File file = new File(path);
         BufferedWriter output;
@@ -27,24 +31,13 @@ public class TextArea extends JTextPane{
         String[] lines = tmp.split("\n");
         txt = Arrays.asList(lines);
     }
-    private void find(String word, SimpleAttributeSet attributeSet) throws BadLocationException {
-        int r = 0;
-        String str = document.getText(0, document.getLength());
 
-        int l = str.indexOf(word);
-        while(l != -1 && r!=-1){
-            r = l + word.length();
-            document.setCharacterAttributes(l, r-l, attributeSet, false);
-            l = str.indexOf(word,r);
-        }
-    }
-    private void findInLine(String word, String line, int start, SimpleAttributeSet attributeSet) throws BadLocationException {
-        int r = 0;
-        int l = line.indexOf(word);
-        while(l != -1 && r!=-1){
-            r = l + word.length();
-            document.setCharacterAttributes(start + l, r - l, attributeSet, true);
-            l = line.indexOf(word,r);
+    private void find(String word, String line, int start, SimpleAttributeSet attributeSet) throws BadLocationException {
+        Pattern pattern = Pattern.compile(word);
+        Matcher matcher = pattern.matcher(line);
+
+        while(matcher.find()){
+            document.setCharacterAttributes(start + matcher.start(), matcher.end() - matcher.start(), attributeSet, false);
         }
     }
     private void syntaxLineHighlight() throws BadLocationException {
@@ -53,34 +46,40 @@ public class TextArea extends JTextPane{
             return;
         }
         int start = getCaretPosition();
-        while(txt.charAt(start - 1) != '\n'){
+        int counter = 0;
+        while (start != 0 && (txt.charAt(start - 1) != '\n' || counter == 0)) {
+            if (txt.charAt(start - 1) != '\n') counter = 1;
             --start;
-            if(start == 0) break;
         }
         int end = getCaretPosition();
-        while(txt.charAt(end-1) != '\n'){
-            if(end == document.getLength()) break;
+        counter = 0;
+        while(end!= document.getLength() && (txt.charAt(end-1) != '\n' || counter==0)){
             ++end;
+            if(txt.charAt(end-1) != '\n') counter = 1;
         }
+
         document.setCharacterAttributes(start, end-start+1, Theme.WA.get(Theme.DEF_WORDS), true);
         String line = txt.substring(start, end);
         for(String word : Theme.WA.get(Theme.OTHER_WORDS).words){
-            findInLine(word, line,start,Theme.WA.get(Theme.OTHER_WORDS));
+            word = "\\b"+word+"\\b";
+            find(word, line,start,Theme.WA.get(Theme.OTHER_WORDS));
         }
         for(String word : Theme.WA.get(Theme.KEY_WORDS).words){
-            findInLine(word, line,start,Theme.WA.get(Theme.KEY_WORDS));
+            word = "\\b"+word+"\\b";
+            find(word, line,start,Theme.WA.get(Theme.KEY_WORDS));
         }
     }
     private void syntaxHighlight() throws BadLocationException {
         document.setCharacterAttributes(0, document.getLength(), Theme.WA.get(Theme.DEF_WORDS), true);
         for(String word : Theme.WA.get(Theme.OTHER_WORDS).words){
-            find(word, Theme.WA.get(Theme.OTHER_WORDS));
+            word = "\\b"+word+"\\b";
+            find(word, document.getText(0, document.getLength()), 0 ,Theme.WA.get(Theme.OTHER_WORDS));
         }
         for(String word : Theme.WA.get(Theme.KEY_WORDS).words){
-            find(word, Theme.WA.get(Theme.KEY_WORDS));
+            word = "\\b"+word+"\\b";
+            find(word, document.getText(0, document.getLength()), 0,Theme.WA.get(Theme.KEY_WORDS));
         }
     }
-    private final DefaultStyledDocument document;
 
     TextArea() throws BadLocationException {
         document = new DefaultStyledDocument();
@@ -97,6 +96,39 @@ public class TextArea extends JTextPane{
                 } catch (BadLocationException badLocationException) {
                     badLocationException.printStackTrace();
                 }
+            }
+            public void keyPressed(KeyEvent e) {
+                if(e.isControlDown()) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_V ->{
+                        try {
+                            syntaxHighlight();
+                        } catch (BadLocationException badLocationException) {
+                            badLocationException.printStackTrace();
+                        }
+                    }
+                    case KeyEvent.VK_O ->{
+                        try {
+                            openFile();
+                        } catch (BadLocationException badLocationException) {
+                            badLocationException.printStackTrace();
+                        }
+                    }
+                    case KeyEvent.VK_S ->{
+                        try {
+                            saveFile();
+                        } catch (BadLocationException badLocationException) {
+                            badLocationException.printStackTrace();
+                        }
+                    }
+                    case KeyEvent.VK_N ->{
+                        try {
+                            newFile();
+                        } catch (BadLocationException badLocationException) {
+                            badLocationException.printStackTrace();
+                        }
+                    }
+                }}
             }
         });
 
@@ -143,25 +175,21 @@ public class TextArea extends JTextPane{
     }
 
     void find(String fword) throws BadLocationException {
-        find(fword, Theme.highlightAttributes);
+        deleteHighlight();
+        find(fword, document.getText(0, document.getLength()), 0,Theme.highlightAttributes);
     }
     void deleteHighlight() throws BadLocationException {
         syntaxHighlight();
     }
     void replace(String fword, String rword) throws BadLocationException {
+        String line = document.getText(0, document.getLength());
 
-        int r = 0;
-        String str = document.getText(0, document.getLength());
+        Pattern pattern = Pattern.compile(fword);
+        Matcher matcher = pattern.matcher(line);
+        String str = matcher.replaceAll(rword);
+        document.remove(0, document.getLength());
+        document.insertString(0, str, null);
 
-        int l = str.indexOf(fword);
-        while(l != -1 && r!=-1){
-            r = l + fword.length();
-            document.remove(l, fword.length());
-            r = l +rword.length();
-            document.insertString(l, rword, null);
-            str = document.getText(0, document.getLength());
-            l = str.indexOf(fword,r);
-        }
         syntaxHighlight();
     }
 
